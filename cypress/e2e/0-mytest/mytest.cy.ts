eb2d90ebed34190f7b6d3a133b072047e0db4a6d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-23 14:18:03 +0200

    dev: test url

--- cypress/e2e/0-mytest/mytest.cy.ts
+++ cypress/e2e/0-mytest/mytest.cy.ts
@@ -3,11 +3,24 @@ describe("add and remove stock articles", () => {
     cy.visit("https://jlg-formation.github.io/cypress-gestion-stock/");
   });
 
-  it("add and remove stock articles", () => {
+  it("check the home page content", () => {
     cy.contains("Gestion Stock");
 
     cy.contains("footer", "Mentions Légales");
     cy.get("main").contains("h1", "Gérer efficacement votre stock");
     cy.get("button, .button").contains("voir le stock", { matchCase: false });
+  });
+
+  it("check the links", () => {
+    cy.get("footer a").click();
+    cy.url().should(
+      "eq",
+      "https://jlg-formation.github.io/cypress-gestion-stock/legal"
+    );
+    cy.go("back");
+    cy.url().should(
+      "eq",
+      "https://jlg-formation.github.io/cypress-gestion-stock/"
+    );
   });
 });
