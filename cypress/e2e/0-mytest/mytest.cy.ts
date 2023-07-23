13cea91bd7fdf3e78e5d89b2e54e3a7ce5efc199
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-23 13:20:33 +0200

    dev: added mytest in ts

---
+++ cypress/e2e/0-mytest/mytest.cy.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("add and remove stock articles", () => {
+  beforeEach(() => {
+    cy.visit("https://jlg-formation.github.io/cypress-gestion-stock/");
+  });
+
+  it("add and remove stock articles", () => {
+    cy.contains("Gestion Stock");
+  });
+});
